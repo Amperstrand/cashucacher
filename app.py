5a4a04a671a6b53746ea8150c0bcab6275bf41c4
amperstrand <amperstrand@localhost>
2023-12-02 15:29:39 +0100

    move encryption to a module

--- app.py
+++ app.py
@@ -6,78 +6,9 @@ import hashlib
 import base64
 import itertools
 
-
-
-
-
-# use this to encrypt the cashu note
-from cryptography.hazmat.primitives import hashes, padding
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from base64 import b64encode, b64decode
-
-def sha256(input_str):
-    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    digest.update(input_str.encode('utf-8'))
-    return digest.finalize()
-
-def encrypt_data(text, key):
-    print("Encrypting:", text)
-    print("With this key (string):")
-    print(key)
-    print("SHA256:")
-    key_hash = sha256(key)
-
-    #not this:
-    ## Limit key size to 32 bytes (256 bits)
-    #key = key_hash[:32]
-    #print('2')
-    #print(key.hex())
-
-    #this works:
-    #key=bytes.fromhex("198a1a4ee777d059d5f8e255deaa7f17")
-    #print(key_hash[:16])
-    key=key_hash[:16]
-    print(key)
-
-
-    # Pad the data using PKCS7
-    padder = padding.PKCS7(128).padder()
-    padded_data = padder.update(text.encode('utf-8')) + padder.finalize()
-
-    # Encrypt the data using AES with ECB mode
-
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
-    encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
-
-    return b64encode(ciphertext).decode('utf-8')
-
-def decrypt_data(encrypted_text, key):
-    print("Decrypting:", encrypted_text)
-    print("With this key (string):")
-    print(key)
-    print("SHA256:")
-    key_hash = sha256(key)
-    key = key_hash[:16]
-    print(key)
-
-    # Decode the Base64 encoded ciphertext
-    ciphertext = b64decode(encrypted_text.encode('utf-8'))
-
-    # Decrypt the data using AES with ECB mode
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
-    decryptor = cipher.decryptor()
-    decrypted_padded_data = decryptor.update(ciphertext) + decryptor.finalize()
-
-    # Unpad the data using PKCS7
-    unpadder = padding.PKCS7(128).unpadder()
-    original_text = unpadder.update(decrypted_padded_data) + unpadder.finalize()
-
-    return original_text.decode('utf-8')
+from encryption_module import encrypt_data, decrypt_data
 
 print(encrypt_data("cashuAeyJ0b2tlbiI6IFt7InByb29mcyI6IFt7ImlkIjogIkkyeU4raVJZZmt6VCIsICJhbW91bnQiOiAxLCAic2VjcmV0IjogIjQzZDgyMGY4NDViNjMyZmNmYThlNjk2YTgzMDRhZTZmMmYwMjE5OWM3Yzg3MTg5YTY3YTA4NzIwZTFkNTlkNzQiLCAiQyI6ICIwM2M5OTg1ZmM0ODIyM2IyMTkwMmU0NjBjN2QxMzcxMDc5MzhjYmU2ZGJiYTVjYmUwYWFkZjNiZDAzNmQ4NTg0M2UifV0sICJtaW50IjogImh0dHBzOi8vODMzMy5zcGFjZTozMzM4LyJ9XX0=", "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AMKJARGV3EXZAE0V9CXJTMKXYHKCMN4WFKZ7DT5WU6HZ4ZXG35K7MJKDG6HX3T4GEGHW4F59AF8JU2TGSMYV6Z3VD58G625WDQ5ZNJDXDU8ZMRH2K0"))
-
 print(decrypt_data("CF4/NQPxqsw4+mCPPgA9+RkfICCLdvU4xS5SFnjUPG8xG3AEv5yVw3kFLItKpbP1/h6Yf7nJt+nEdHHQ7h4ZJIWeB6iDka+Nr9fQRxOkZid4R+mwkywomq2JsfcvySSjuAI8ZU2IZANBZVxrg9E596C1V8EOlH4GCAyXMKdr2viSoOns/1ejOu/yPRjXlkYLllIZIh5uc822D5ghUU9GilYlCYfa7BeDBT8m9ki6Eswu7Nr4HWbFHDjMDUnE+VD2xZdpYIXt6Bnm4ip4MbaSAX/fA8J6tcmmOhkn0x//oOkhFHBfEK/b2vB+Ph9zGIWKpIk9WIH1q/5Fdjm579Tq5zahP65jDK5C7pryX260o/THORtBCJ0/2R2WL/qPqPx5pFMGFshc2YcgmWCk/grcvvuf3nyZdWbF/K5NjicPAz9G4oazHvKrpbC0mOiIaKxmASUDbVZ7lqzjyjOYsrYP3Q==", "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AMKJARGV3EXZAE0V9CXJTMKXYHKCMN4WFKZ7DT5WU6HZ4ZXG35K7MJKDG6HX3T4GEGHW4F59AF8JU2TGSMYV6Z3VD58G625WDQ5ZNJDXDU8ZMRH2K0"))
 
 app = Flask(__name__)
@@ -108,7 +39,6 @@ def read_decrypted_note(offset):
 
 # Index to keep track of the current position
 current_index = 0
-
 
 # Generate encrypted nuts for each lnurlw
 
---
+++ encryption_module.py
@@ -0,0 +1,65 @@
+from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from base64 import b64encode, b64decode
+
+
+def sha256(input_str):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(input_str.encode('utf-8'))
+    return digest.finalize()
+
+def encrypt_data(text, key):
+    print("Encrypting:", text)
+    print("With this key (string):")
+    print(key)
+    print("SHA256:")
+    key_hash = sha256(key)
+
+    #not this:
+    ## Limit key size to 32 bytes (256 bits)
+    #key = key_hash[:32]
+    #print('2')
+    #print(key.hex())
+
+    #this works:
+    #key=bytes.fromhex("198a1a4ee777d059d5f8e255deaa7f17")
+    #print(key_hash[:16])
+    key=key_hash[:16]
+    print(key)
+
+
+    # Pad the data using PKCS7
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(text.encode('utf-8')) + padder.finalize()
+
+    # Encrypt the data using AES with ECB mode
+
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+    return b64encode(ciphertext).decode('utf-8')
+
+def decrypt_data(encrypted_text, key):
+    print("Decrypting:", encrypted_text)
+    print("With this key (string):")
+    print(key)
+    print("SHA256:")
+    key_hash = sha256(key)
+    key = key_hash[:16]
+    print(key)
+
+    # Decode the Base64 encoded ciphertext
+    ciphertext = b64decode(encrypted_text.encode('utf-8'))
+
+    # Decrypt the data using AES with ECB mode
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+    # Unpad the data using PKCS7
+    unpadder = padding.PKCS7(128).unpadder()
+    original_text = unpadder.update(decrypted_padded_data) + unpadder.finalize()
+
+    return original_text.decode('utf-8')
